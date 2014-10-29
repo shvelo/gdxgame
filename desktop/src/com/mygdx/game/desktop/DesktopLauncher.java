67703c7fc7c60de55fdc8c7ac3a74aad418b9e56
Nick Shvelidze <captain@pirrate.me>
2014-10-29 13:48:07 +0400

    Update

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -4,11 +4,18 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
 
+import java.awt.Toolkit;
+import java.awt.Dimension;
+
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.fullscreen = true;
 		config.x = -1;
 		config.y = -1;
+		config.width = (int) screenSize.getWidth();
+		config.height = (int) screenSize.getHeight();
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
